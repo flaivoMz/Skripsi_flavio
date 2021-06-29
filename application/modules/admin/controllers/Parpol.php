652aeef73249381a140e46f6551fd332fdddff9f
abdulhamid07 <abdulhamid.dev@gmail.com>
2021-06-29 10:55:37 +0700

    add bab 4 on prog

--- application/modules/admin/controllers/Parpol.php
+++ application/modules/admin/controllers/Parpol.php
@@ -3,65 +3,64 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Parpol extends MX_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        is_logged_in_admin();
-        $this->load->model('ParpolModel');
+  public function __construct()
+  {
+    parent::__construct();
+    is_logged_in_admin();
+    $this->load->model('ParpolModel');
+  }
+
+  public function index($id = null)
+  {
+    $data['title'] = 'Data Partai Politik';
+    $data['parpol'] = $this->ParpolModel->semuaParpol();
+    if ($id) {
+      $data['detail'] = $this->ParpolModel->parpolById($id);
     }
+    adminView('parpol/index', $data);
+  }
 
-    public function index($id = null)
-    {
-        $data['title'] = 'Data Partai Politik';
-        $data['parpol'] = $this->ParpolModel->semuaParpol();
-        if($id){
-            $data['detail'] = $this->ParpolModel->parpolById($id);
-        }
-        adminView('parpol/index', $data);
-    }
+  public function simpan_parpol()
+  {
+    $this->form_validation->set_rules('nama_parpol', 'Nama partai', 'trim|required');
+    $this->form_validation->set_rules('singkatan', 'Singkatan', 'trim|required');
 
-    public function simpan_parpol()
-    {
-        $this->form_validation->set_rules('nama_parpol', 'Nama partai', 'trim|required');
-        $this->form_validation->set_rules('singkatan', 'Singkatan', 'trim|required');
+    if ($this->form_validation->run() == FALSE) {
+      redirect('admin/parpol');
+    } else {
+      if ($this->input->post('submit') == "Tambah") {
 
-        if ($this->form_validation->run() == FALSE) {
-            redirect('admin/parpol');
+        if ($this->ParpolModel->tambahParpol()) {
+          $this->session->set_flashdata('success', 'Data Partai Berhasil Ditambah');
         } else {
-            if ($this->input->post('submit') == "Tambah") {
-
-                if ($this->ParpolModel->tambahParpol()) {
-                    $this->session->set_flashdata('success', 'Data Partai Berhasil Ditambah');
-                } else {
-                    $this->session->set_flashdata('danger', 'Data Partai Gagal Ditambah');
-                }
-            }else{
-                if ($this->ParpolModel->editParpol()) {
-                    $this->session->set_flashdata('success', 'Data Partai Berhasil Diedit');
-                } else {
-                    $this->session->set_flashdata('danger', 'Data Partai Gagal Diedit');
-                }
-            }
-            redirect('admin/parpol');
+          $this->session->set_flashdata('danger', 'Data Partai Gagal Ditambah');
         }
-    }
-
-    public function hapus_parpol($id_parpol)
-    {
-        $parpol = $this->ParpolModel->parpolById($id_parpol);
-
-        if ($parpol) {
-            if ($this->ParpolModel->hapusParpol($id_parpol)) {
-                if($parpol['logo'] != "defaultlogo.png"){
-                    $img_file = "./assets/images/parpol/" . $parpol['logo'];
-                    unlink($img_file);
-                }
-                $this->session->set_flashdata('success', 'Data Partai Berhasil Dihapus');
-            }else{
-                $this->session->set_flashdata('success', 'Data Partai Gagal Dihapus');
-            }
+      } else {
+        if ($this->ParpolModel->editParpol()) {
+          $this->session->set_flashdata('success', 'Data Partai Berhasil Diedit');
+        } else {
+          $this->session->set_flashdata('danger', 'Data Partai Gagal Diedit');
         }
-        redirect('admin/parpol');;
+      }
+      redirect('admin/parpol');
     }
-    
+  }
+
+  public function hapus_parpol($id_parpol)
+  {
+    $parpol = $this->ParpolModel->parpolById($id_parpol);
+
+    if ($parpol) {
+      if ($this->ParpolModel->hapusParpol($id_parpol)) {
+        if ($parpol['logo'] != "defaultlogo.png") {
+          $img_file = "./assets/images/parpol/" . $parpol['logo'];
+          unlink($img_file);
+        }
+        $this->session->set_flashdata('success', 'Data Partai Berhasil Dihapus');
+      } else {
+        $this->session->set_flashdata('success', 'Data Partai Gagal Dihapus');
+      }
+    }
+    redirect('admin/parpol');;
+  }
 }
