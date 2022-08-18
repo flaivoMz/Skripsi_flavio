2c87031fc695d3a4e0fff5ceb61be2cc84a26a6f
Afendi Nahak <flaviomoniz96@gmail.com>
2022-08-18 21:43:13 +0700

    first commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/skripsi_flavio/';
+$config['base_url'] = 'http://localhost/skripsiflavio/';
 // $config['base_url'] = 'http://skripsi-flavio.atwebpages.com/';
 
 /*
